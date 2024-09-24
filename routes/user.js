bcc1b6403fdfa837981e042efc4755ad16899778
Wasim Zaman <wasimxaman13@gmail.com>
2024-09-24 18:58:41 +0500

    Refactor user routes to require admin privileges for registration, updating, and deleting users

--- routes/user.js
+++ routes/user.js
@@ -2,16 +2,17 @@ const express = require('express');
 
 const userController = require('../controllers/user');
 const isAuth = require('../middleware/isAuth');
+const isAdmin = require('../middleware/isAdmin');
 
 const router = express.Router();
 
 // Version 1 routes
-router.post('/v1/register', userController.register);
+router.post('/v1/register', isAdmin, userController.register);
 router.post('/v1/login', userController.login);
 router.get('/v1/getUser', isAuth, userController.getUser);
 router.get('/v1/getUser/:id', userController.getUserById);
-router.put('/v1/updateUser/:id', userController.updateUserById);
-router.delete('/v1/deleteUser/:id', userController.deleteUserById);
+router.put('/v1/updateUser/:id', isAdmin, userController.updateUserById);
+router.delete('/v1/deleteUser/:id', isAdmin, userController.deleteUserById);
 router.get('/v1/getUsers', userController.getUsers);
 
 module.exports = router;
